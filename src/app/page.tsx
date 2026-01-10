15af23b7be011d27596499b48b8533566dfafa46
mirjSolution <ricojohndatoon23@gmail.com>
2026-01-09 22:18:33 -0500

    06: Background Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,34 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-const Page = async () => {  
-  await requireAuth()
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getworkflows.queryOptions());
 
-  const data = await caller.getUsers()
+  const create = useMutation(
+    trpc.createWorkFlow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued");
+      },
+    })
+  );
 
-  return  <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
-   protected server component
-   <div>
-    {JSON.stringify(data, null, 2)}
-   </div>
-   <LogoutButton />
-  </div>
-}
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      protected server component
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
+      <LogoutButton />
+    </div>
+  );
+};
 
 export default Page;
