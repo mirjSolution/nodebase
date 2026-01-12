87986f96ffd3cdc4b4223567cd261b0bed9953a3
mirjSolution <ricojohndatoon23@gmail.com>
2026-01-11 22:44:41 -0500

    10: Payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,34 @@
+import { polar, checkout, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: 'postgresql'
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "3fd7c57a-0a68-477d-af03-95e91eb30cd2",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
     }),
-    emailAndPassword: {
-        enabled: true,
-        autoSignIn: true
-    }
+  ],
 });
-
