f169fdf14de2edfeba143bffc16d1407fd0d6e8b
mirjSolution <ricojohndatoon23@gmail.com>
2026-01-25 21:30:36 -0500

    20:Node Templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,13 +1,19 @@
+import Handlebars from "handlebars";
 import type { NodeExecutor } from "@/features/executions/types";
-import { da } from "date-fns/locale";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
 
+Handlebars.registerHelper("json", (context) => {
+  const jsonString = JSON.stringify(context, null, 2);
+  const safeString = new Handlebars.SafeString(jsonString);
+  return safeString;
+});
+
 type HttpRequestData = {
-  variableName?: string;
+  variableName: string;
   endpoint: string;
   method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-  body?: any;
+  body?: string;
 };
 
 export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
@@ -21,17 +27,25 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
   }
 
   if (!data.variableName) {
-    throw new NonRetriableError("Variable name is not configured");
+    throw new NonRetriableError(
+      "HTTP Request node: Variable name is not configured",
+    );
+  }
+
+  if (!data.method) {
+    throw new NonRetriableError("HTTP Request node: Method is not configured");
   }
 
   const result = await step.run("http_request", async () => {
-    const endpoint = data.endpoint!;
-    const method = data.method || "GET";
+    const endpoint = Handlebars.compile(data.endpoint)(context);
+    const method = data.method;
 
     const options: KyOptions = { method };
 
     if (["POST", "PUT", "PATCH"].includes(method)) {
-      options.body = data.body;
+      const resolved = Handlebars.compile(data.body || "{}")(context);
+      JSON.parse(resolved); // Validate JSON
+      options.body = resolved;
       options.headers = {
         "Content-Type": "application/json",
       };
@@ -51,16 +65,10 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
       },
     };
 
-    if (data.variableName) {
-      return {
-        ...context,
-        [data.variableName!]: responsePayload,
-      };
-    }
     // Fallback to direct httpRespnse for backward compatibility
     return {
       ...context,
-      ...responsePayload,
+      [data.variableName]: responsePayload,
     };
   });
   return result;
